2ce8bc0cab0411f727d9999c85ef24a3cfb4c371
seojin <seojin@gmail.com>
2018-12-10 19:09:22 +0900

    day02

--- app/src/main/java/com/example/seojin/doit_android/MainActivity.java
+++ app/src/main/java/com/example/seojin/doit_android/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.seojin.doit_android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v){
+        Toast.makeText(getApplicationContext(), "버튼이 눌렸어요~", Toast.LENGTH_LONG).show();
+    }
+
+    public void onButton2Clicked(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(intent);
+    }
+
+    public void onButton3Clicked(View v){
+
     }
 }
