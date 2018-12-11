d4cc1dbe9a67c69fb803d30da460893c95fc2ee3
wiv33 <wiv.psk@gmail.com>
2018-12-11 23:22:13 +0900

    오타 수정, Button의 Layout 수정해보기. wrap_content, match_parent, match_constraint

--- app/src/main/java/com/study/psk/doit_android/MainActivity.java
+++ app/src/main/java/com/study/psk/doit_android/MainActivity.java
@@ -5,9 +5,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
-
-import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
